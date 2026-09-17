cdf69b2a714352018c560ff8b188717738b59dbc
unknown <nhagman-creator>
2026-09-17 15:45:01 +0200

    Added function to check for valid user input as string

--- ShoppingList.cs
+++ ShoppingList.cs
@@ -2,3 +2,31 @@
 //Creator:  Arne Hagman
 //Date:     17Sep2026 
 //Course:   MAI26MA - Module II - Programmering och objektorinterad utveckling in C#
+
+//external variables declaration
+string question1 = "Enter product name: ";
+string question2 = "Enter the Price (integer): ";
+string? inputProduct = "Empty";
+int inputPrice = 0;
+
+//Function to check for valid user input (string). Parameter = question, return true or false.
+bool checkInputIsString(string question) //OOP - could be a base class for checking input
+{
+    Console.Write(question);
+    
+    //Allow null value
+    string? input = Console.ReadLine();
+
+    //Check for invalid input characters (null/whitespace)
+    //if(input != "")
+    //Check for invalid input characters (null/whitespace)
+    if(!string.IsNullOrWhiteSpace(input))
+    {
+        inputProduct=input;
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
